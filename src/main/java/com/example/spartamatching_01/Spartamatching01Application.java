65e47c222ceb10c2c95c9f4320983f3e08b7d8e3
Son Hyeeun <guswlsrkdmf@naver.com>
2023-01-17 16:56:38 +0900

    new

---
+++ src/main/java/com/example/spartamatching_01/Spartamatching01Application.java
@@ -0,0 +1,16 @@
+package com.example.spartamatching_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Spartamatching01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spartamatching01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spartamatching_01/Spartamatching01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartamatching_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spartamatching01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
