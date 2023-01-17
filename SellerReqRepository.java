faccaf2e658ae073c43a4e3d061fa91a7ed7139b
dripmaster <117059353+Dripmaster44@users.noreply.github.com>
2023-01-17 20:32:53 +0900

    Add files via upload

---
+++ SellerReqRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import com.example.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SellerReqRepository extends JpaRepository<Client, Long> {
+
+    List<Client> ClientReqList();
+
+
+}
