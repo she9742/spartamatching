dd044b78c2dc8f7b3e7342ab8057a5e08287dd61
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-01-18 15:50:38 +0900

    refactoring1

---
+++ src/main/java/com/example/dto/ProfileUpdateRequestDto.java
@@ -0,0 +1,13 @@
+import com.example.entity.Client;
+import lombok.Getter;
+
+@Getter
+public class ProfileUpdateRequestDto {
+    private String nickname;
+    private String image;
+
+    public ProfileUpdateRequestDto(Client client) {
+        this.nickname = client.getNickname();
+        this.image = client.getImage();
+    }
+}
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -4,9 +4,9 @@ import com.example.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    Optional<Message> findAllByTalk(Long talkId);
+    List<Message> findAllByTalk(Long talkId);
 }
