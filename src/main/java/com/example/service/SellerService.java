9b115f6e8bdeedb2eb282befe0a85c9c66404616
DRIPMASTER <qsccsq4444@naver.com>
2023-01-18 18:09:03 +0900

    <refactoring> sellProduct 구현

--- src/main/java/com/example/service/SellerService.java
+++ src/main/java/com/example/service/SellerService.java
@@ -2,11 +2,8 @@ package com.example.service;
 
 import com.example.dto.ProductRequestDto;
 import com.example.dto.ProductResponseDto;
-import com.example.entity.ClientReq;
-import com.example.entity.Product;
-import com.example.entity.Talk;
-import com.example.repository.ClientReqRepository;
-import com.example.repository.ProductRepository;
+import com.example.entity.*;
+import com.example.repository.*;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,48 +19,54 @@ public class SellerService {
 
     private final ProductRepository productRepository;
     private final ClientReqRepository clientReqRepository;
+    private final ClientRepository clientRepository;
+
+    private final TradeReqRepository tradeReqRepository;
+
+    private final TalkRepository talkRepository;
 
     @Transactional
-    public void getProfile(){
+    public void getProfile() {
 
     }
 
     @Transactional
-    public void getMyProduct(){
+    public void getMyProduct() {
 
     }
 
     @Transactional
-    public void getMyClientReq(){
+    public void getMyClientReq() {
 
     }
 
     @Transactional
-    public void enrollMyProduct(){
+    public void enrollMyProduct() {
 
     }
 
     @Transactional
-    public void updateMyProduct(){
+    public void updateMyProduct() {
 
     }
 
     @Transactional
-    public void deleteMyProduct(){
+    public void deleteMyProduct() {
 
     }
 
     @Transactional
-    public ResponseEntity<List<ClientReq>> getMatching(Long sellerId){
+    public ResponseEntity<List<ClientReq>> getMatching(Long sellerId) {
         // 매칭이 들어왔는지 그러니까 매칭 요청 목록(clientReq)을 확인할 수 있다.
         //내가 셀러 아이디에 해당하는 매칭 요청 목록만 리턴해주고
         //-> 게시글 조회? 전체 게시글 조회인데 그 아이디가 셀러아이디인 경우
-       List<ClientReq> clientReq = clientReqRepository.findAllBySellerId(sellerId);
-       return ResponseEntity.ok().body(clientReq);
+        List<ClientReq> clientReq = clientReqRepository.findAllBySellerId(sellerId);
+        return ResponseEntity.ok().body(clientReq);
 
     }
+
     @Transactional
-    public String approveMatching(ClientReq clientReq){
+    public String approveMatching(ClientReq clientReq) {
         //승인해줄 클라이언트 아이디값을 넣어주고
         // 승인이 되면 클라이언트req가 사라지고 대화방이 열린다.
         //반환값은 String으로 간단하게 해주고 대화방이다
@@ -74,37 +77,65 @@ public class SellerService {
     }
 
     @Transactional
-    public void sellProduct(){
-    }
-    @Transactional
-    public void deposit(){
+    public void getTradeReq(Long sellerId) {
+        List<TradeReq> tradeReqs = tradeReqRepository.findAllBySellerId(sellerId);
     }
 
     @Transactional
-    public void openTalk(){
+    public void sellProduct(TradeReq tradeReq, Long productId, Long talkId) {
+        Client client = clientRepository.findById(tradeReq.getClientId()).orElseThrow(
+                () -> new IllegalArgumentException("")
+        );
+        Client seller = clientRepository.findById(tradeReq.getSellerId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 판매자가 존재하지 않습니다.")
+        );
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new IllegalArgumentException("해당 상품이 존재하지 않습니다.")
+        );
+        Talk talk = talkRepository.findById(talkId).orElseThrow(
+                () -> new IllegalArgumentException("")
+        );
+        // 1. 물건이 판매되면 product,tradeReq 를 삭제한다.
+        productRepository.deleteById(productId);
+        tradeReqRepository.delete(tradeReq);
+
+        // 2. 대화창을 닫는다.
+        talk.closeTalk();
+
+        // 3. point를 옮긴다.
+        client.withdraw(product.getPoint());
+        seller.deposit(product.getPoint());
+    }
+
+    @Transactional
+    public void deposit() {
+    }
+
+    @Transactional
+    public void openTalk() {
         //승인이 되는 순간 바로 대화방이 열린다.
     }
 
     @Transactional
-    public void sendMessage(){
+    public void sendMessage() {
 
     }
 
     @Transactional
-    public void getMessage(){
+    public void getMessage() {
 
     }
 
     @Transactional
-    public void closeTalk(){
+    public void closeTalk() {
 
     }
 
     // 나의 판매상품 수정
     @Transactional
-    public ProductResponseDto updateProduct(Long id, ProductRequestDto requestDto){
+    public ProductResponseDto updateProduct(Long id, ProductRequestDto requestDto) {
         Product product = productRepository.findById(id).orElseThrow(
-            ()-> new IllegalArgumentException("수정하려는 상품이 없습니다.")
+                () -> new IllegalArgumentException("수정하려는 상품이 없습니다.")
         );
         product.update(requestDto);
         return new ProductResponseDto(product);
@@ -112,9 +143,9 @@ public class SellerService {
 
 
     // 판매상품 삭제
-    public void deleteProduct(Long id){
+    public void deleteProduct(Long id) {
         Product product = productRepository.findById(id).orElseThrow(
-            ()-> new IllegalArgumentException("수정하려는 상품이 없습니다.")
+                () -> new IllegalArgumentException("수정하려는 상품이 없습니다.")
         );
         product.unactivate();
     }
