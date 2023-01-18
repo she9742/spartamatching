7969cc37f3338d84afa35bf812617008970790d0
DRIPMASTER <qsccsq4444@naver.com>
2023-01-18 17:46:26 +0900

    <refactoring> sellProduct 구현

--- src/main/java/com/example/repository/TradeReqRepository.java
+++ src/main/java/com/example/repository/TradeReqRepository.java
@@ -5,9 +5,12 @@ import com.example.entity.TradeReq;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TradeReqRepository extends JpaRepository<TradeReq,Long> {
-    Optional<TradeReq> findBySellerId(Long sellerId);
+    Optional<TradeReq> findBySellerIdAndClientId(Long sellerId,Long clientId);
+
+    List<TradeReq> findAllBySellerId(Long sellerId);
 }
--- src/main/java/com/example/service/SellerService.java
+++ src/main/java/com/example/service/SellerService.java
@@ -1,8 +1,29 @@
 package com.example.service;
 
+import com.example.entity.Client;
+import com.example.entity.Product;
+import com.example.entity.Talk;
+import com.example.entity.TradeReq;
+import com.example.repository.ClientRepository;
+import com.example.repository.ProductRepository;
+import com.example.repository.TalkRepository;
+import com.example.repository.TradeReqRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SellerService {
+    private final ClientRepository clientRepository;
+
+    private final ProductRepository productRepository;
+
+    private final TradeReqRepository tradeReqRepository;
+
+    private final TalkRepository talkRepository;
 
     @Transactional
     public void getProfile(){
@@ -40,8 +61,34 @@ public class SellerService {
     }
 
     @Transactional
-    public void sellProduct(){
+    public void getTradeReq(Long sellerId){
+        List<TradeReq> tradeReqs = tradeReqRepository.findAllBySellerId(sellerId);
+    }
 
+    @Transactional
+    public void sellProduct(TradeReq tradeReq, Long productId, Long talkId){
+        Client client = clientRepository.findById(tradeReq.getClientId()).orElseThrow(
+                () -> new IllegalArgumentException("")
+        );
+        Client seller = clientRepository.findById(tradeReq.getSellerId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 판매자가 존재하지 않습니다.")
+        );
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new IllegalArgumentException("해당 상품이 존재하지 않습니다.")
+        );
+        Talk talk = talkRepository.findById(talkId).orElseThrow(
+                () -> new IllegalArgumentException("")
+        );
+        // 1. 물건이 판매되면 product,tradeReq 를 삭제한다.
+        productRepository.deleteById(productId);
+        tradeReqRepository.delete(tradeReq);
+
+        // 2. 대화창을 닫는다.
+        talk.closeTalk();
+
+        // 3. point를 옮긴다.
+        client.withdraw(product.getPoint());
+        seller.deposit(product.getPoint());
     }
 
     @Transactional
@@ -63,12 +110,5 @@ public class SellerService {
     public void getMessage(){
 
     }
-
-    @Transactional
-    public void closeTalk(){
-
-    }
-
-
 
 }
