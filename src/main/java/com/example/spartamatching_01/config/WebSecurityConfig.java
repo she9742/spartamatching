6dc4ccff3d74cfa5c446323b4234782b89a7cdf9
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-01-21 20:21:18 +0900

    refactoring

--- src/main/java/com/example/spartamatching_01/config/WebSecurityConfig.java
+++ src/main/java/com/example/spartamatching_01/config/WebSecurityConfig.java
@@ -4,12 +4,12 @@ import com.example.exception.CustomAccessDeniedHandler;
 import com.example.exception.CustomAuthenticationEntryPoint;
 import com.example.jwt.JwtAuthFiler;
 import com.example.jwt.JwtUtil;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -30,8 +30,8 @@ public class WebSecurityConfig {
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
-    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
-
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();}
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -42,22 +42,20 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.csrf().disable();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests().antMatchers("/client/signup").permitAll()
-                                .antMatchers("/client/signin").permitAll()
-                                .antMatchers("/admin/signup").permitAll()
-                                .antMatchers("/admin/signin").permitAll()
-                                .antMatchers("/h2-console/**").permitAll()
-                                .antMatchers("/client/**").permitAll()
-                                .antMatchers("/seller/**").permitAll()
-                                .antMatchers("/admin/**").permitAll()
-                                .anyRequest().authenticated()
-                                .and().addFilterBefore(new JwtAuthFiler(jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
+                .antMatchers("/client/signin").permitAll()
+                .antMatchers("/admin/signup").permitAll()
+                .antMatchers("/admin/signin").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/client/**").permitAll()
+                .antMatchers("/seller/**").permitAll()
+                .antMatchers("/admin/**").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(new JwtAuthFiler(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         http.formLogin().disable();
         // 401 Error 처리, Authorization 즉, 인증과정에서 실패할 시 처리
         http.exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint);
